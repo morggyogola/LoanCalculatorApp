4cc445b776d6a0003abd2fcf0eb3fc6bd3423948
Morgan  Ogola <morgy@Morgans-MacBook-Pro.local>
2026-05-22 12:57:23 +0300

    Loan app project initialization

---
+++ LoanCalculatorApp/LoanCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoanCalculatorAppApp.swift
+//  LoanCalculatorApp
+//
+//  Created by Morgan  Ogola on 22/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct LoanCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
