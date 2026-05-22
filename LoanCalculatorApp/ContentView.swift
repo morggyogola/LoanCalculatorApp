e1a4552a0ebe4117846051fe71b6aed3edb50bde
Morgan  Ogola <morgy@Morgans-MacBook-Pro.local>
2026-05-22 13:40:27 +0300

    Added loan application page with navbar and available loans card

--- LoanCalculatorApp/ContentView.swift
+++ LoanCalculatorApp/ContentView.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-                    }
-        .padding()
+        ScrollView {
+            VStack(spacing: 20) {
+                NavBarView()
+                    
+                
+                Text("Available Loans")
+                    .font(.subheadline)
+                    .padding(.top, 20)
+                
+                LoanCardView(title: "Salary E-Loan", subtitle: "Get quick loans to boost your income", bgColor: .green)
+                
+                LoanCardView(title: "Buy Now Pay Later", subtitle: "Buy goods today, pay later", bgColor: .blue)
+                
+                LoanCardView(title: "Stock Loan", subtitle: "Boost your business stock today", bgColor: .brown)
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
