81c432567bb5c93311d4cf77d5dd5e1fa0a4fe17
Morgan  Ogola <morgy@Morgans-MacBook-Pro.local>
2026-05-22 15:39:46 +0300

    Added styling to navbar

--- LoanCalculatorApp/View/NavBarView.swift
+++ LoanCalculatorApp/View/NavBarView.swift
@@ -10,24 +10,29 @@ import SwiftUI
 struct NavBarView: View {
     var body: some View {
         HStack {
-            
-            Image("person.crop.circle.fill")
+            Image(systemName: "person.circle.fill")
                 .resizable()
                 .frame(width: 50, height: 50)
+                .foregroundColor(.white)
                 .padding(.leading)
             
-            VStack {
+            VStack(alignment: .leading, spacing: 2) {
                 Text("Hello There!")
-                    .font(.title)
+                    .font(.title2)
+                    .bold()
                 Text("Boost your income today!")
-                    .font(.subheadline)
+                    .font(.caption)
             }
             .foregroundColor(.white)
+            .padding(.leading, 10)
+            
+            Spacer()
         }
+        .padding(.vertical, 20)
         .frame(maxWidth: .infinity)
-        .background(Color.green)
+        .background(Color(red: 0.0, green: 0.35, blue: 0.2)) // A darker, more visible green
+        .edgesIgnoringSafeArea(.top)
     }
-    
 }
 
 #Preview {
