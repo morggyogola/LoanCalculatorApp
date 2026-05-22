f87edd2f53567629e554b8e5e28aeea4c828fcf6
Morgan  Ogola <morgy@Morgans-MacBook-Pro.local>
2026-05-22 14:24:00 +0300

    Added the navigation view for the cards on click navigate to apply loan page

--- LoanCalculatorApp/ContentView.swift
+++ LoanCalculatorApp/ContentView.swift
@@ -9,23 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            VStack(spacing: 20) {
-                NavBarView()
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 20) {
+                    NavBarView()
                     
-                
-                Text("Available Loans")
-                    .font(.subheadline)
-                    .padding(.top, 20)
-                
-                LoanCardView(title: "Salary E-Loan", subtitle: "Get quick loans to boost your income", bgColor: .green)
-                
-                LoanCardView(title: "Buy Now Pay Later", subtitle: "Buy goods today, pay later", bgColor: .blue)
-                
-                LoanCardView(title: "Stock Loan", subtitle: "Boost your business stock today", bgColor: .brown)
+                    Text("Available Loans")
+                        .font(.subheadline)
+                        .padding(.top, 20)
+                    
+                    NavigationLink(destination: ApplyLoanView()) {
+                        LoanCardView(title: "Salary E-Loan", subtitle: "Get quick loans to boost your income", bgColor: .green)
+                    }
+                    
+                    NavigationLink(destination: ApplyLoanView()) {
+                        LoanCardView(title: "Buy Now Pay Later", subtitle: "Buy goods today, pay later", bgColor: .blue)
+                    }
+                    
+                    NavigationLink(destination: ApplyLoanView()) {
+                        LoanCardView(title: "Stock Loan", subtitle: "Boost your business stock today", bgColor: .brown)
+                    }
+                }
             }
+            .edgesIgnoringSafeArea(.top)
+            .navigationBarHidden(true)
         }
-        .edgesIgnoringSafeArea(.top)
     }
 }
 
